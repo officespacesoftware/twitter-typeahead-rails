8ffc2cd92d704756bef70dd0d0af44afe83b4e82
Mauricio Gonzalez <mhenlon@gmail.com>
2015-09-17 15:24:08 -0600

    patch for remote completion bug #2

--- vendor/assets/javascripts/twitter/typeahead/typeahead.bundle.js
+++ vendor/assets/javascripts/twitter/typeahead/typeahead.bundle.js
@@ -1720,9 +1720,9 @@
                     suggestions = suggestions || [];
                     if (!canceled && rendered < that.limit) {
                         that.cancel = $.noop;
-                        suggestions = suggestions.slice(0, that.limit - rendered);
                         rendered += suggestions.length;
-                        that._append(query, suggestions);
+                        that._append(query, suggestions.slice(0, that.limit - rendered));
+                        that.async && that.trigger("asyncReceived", query);
                         that.async && that.trigger("asyncReceived", query);
                     }
                 }
